a7057a834452104d0b54e8cda2097d98a4ab99ac
Pasieka Manuel <mapa17@posgrado.upv.es>
2013-07-12 12:41:51 +0200

    Change Protocol to continously read from Serial Port and periodically send out messages

--- Knuspi.cpp
+++ Knuspi.cpp
@@ -2,9 +2,25 @@
 #include <stdlib.h>
 
 #include <windows.h>
-
 #include "rs232.h"
 
+double ClockFrequency;
+
+double getTime()
+{
+	double t;
+	LARGE_INTEGER li;
+	
+	if( !QueryPerformanceCounter( &li ) ){
+		fprintf(stderr, "Quering the Performance Counter Fails!");
+	} else {		
+		t = li.QuadPart;
+		t = t / ClockFrequency;		
+	}
+	return t;
+}
+
+#define MAX_BUFFER_SIZE 100
 int main( int argc, char** argv)
 {
 	int portId;
@@ -21,6 +37,18 @@ int main( int argc, char** argv)
 		portId = atoi( argv[1] );
 		nChars2Send = atoi( argv[2] );
 		sleep_time = atoi( argv[3] );
+		
+		if( nChars2Send >= MAX_BUFFER_SIZE )
+		{
+			fprintf(stderr, "TEST_STRING_LEGNTH too long. Max %d\n", MAX_BUFFER_SIZE);
+			exit(1);
+		}
+		
+		if( sleep_time <= 10 )
+		{
+			fprintf(stderr, "SLEEP_IN_MS too low! Min %g\n", 10.0 );
+			exit(1);
+		}
 	}
 	
 	RS232_comportIdx2Name( portId, portName );
@@ -35,18 +63,20 @@ int main( int argc, char** argv)
 	#define N 10
 	double out;
 	double in;
+	double now;
 	double tDiff[N];
-	double freq;
-	unsigned char b[100];	
-	memset(b, 0, 6);
+	int tCnter = -1;
+	unsigned char b[MAX_BUFFER_SIZE];	
+	int bIdx;
 	int nBytes;
+	double lastTimeMsgSend;	
 	
 	LARGE_INTEGER li;
 	if(!QueryPerformanceFrequency(&li)){
 		fprintf(stderr, "QueryPerformanceFrequency failed!\n");
 	} else {			
-		freq = double(li.QuadPart)/1000.0; //Scale to ms granularity		
-		printf("Timer running at %g hz , div %g\n", (double)(li.QuadPart), freq);
+		ClockFrequency = double(li.QuadPart)/1000.0; //Scale to ms granularity		
+		printf("Timer running at %g hz , div %g\n", (double)(li.QuadPart), ClockFrequency);
 	}
 	
 			
@@ -58,63 +88,71 @@ int main( int argc, char** argv)
 	
 	printf("Entering main busy loop, hit Ctrl-C to stop program ...\n");
 	
+	bIdx = 0;
+	lastTimeMsgSend = getTime();
 	while(1)
 	{
-		for( int i = 0; i < N ; i++ )
-		{			
-			Sleep( sleep_time );
-            for(int j=0; j < nChars2Send; j++){
-                RS232_SendByte( portId, 'a'+i );
-            }
 				
-			if( !QueryPerformanceCounter( &li ) ){
-				fprintf(stderr, "Quering the Performance Counter Fails!");
-			} else {
-				out = li.QuadPart;
-				out = out / freq;
-				//out = (double) ( (double)(li.QuadPart) / freq );
-			}
-					
-			int it = 0;					
-			do{
-				nBytes = RS232_PollComport( portId, b, nChars2Send);
-				it++;
-				if(it > 10000){
-					printf("Break\n");
-					break;		
-				}					
-			}while( !nBytes );
+		Sleep( 1 );
+		nBytes = RS232_PollComport( portId, &(b[bIdx]), nChars2Send);		
+		bIdx += nBytes;
+		
+		//if( nBytes ){
+		//	printf("R %d , bIdx %d\n", nBytes, bIdx );
+		//}
+				
+		//Periodically Send messages
+		now = getTime();
+		if( (now - lastTimeMsgSend) > sleep_time )
+		{					
+			//printf("S ... \n");
+			for(int j=0; j < nChars2Send; j++){
+				RS232_SendByte( portId, '#'+j );
+			}								
+			out = getTime();
+			lastTimeMsgSend = out;
+		}
+						
+		//Detect if this response is an echo
+		if( (b[0] == '#'+1) && ( bIdx >= nChars2Send ) )
+		{
+			//printf("Found a package ...\n");
+			in = getTime();
+			tCnter++;
+			tDiff[tCnter] =  in - out;
 			
-			if( nBytes != nChars2Send ) {
-				printf("Wrong number of received bytes! %d\n", nBytes);
-			} else {
-				if( !QueryPerformanceCounter( &li ) ){
-					fprintf(stderr, "Quering the Performance Counter Fails!");
-				} else {
-					in = (double) ( (double)(li.QuadPart) / freq );
-					in = li.QuadPart;
-					in = in / freq;
-					tDiff[i] =  in - out;
-					//printf("diff %g , in %g , out %g\n", tDiff[i] , in , out);			
-				}
-				if( b[0] != ('a'+i+1) ){
-					printf("Strange Data! %s , expect [%c]\n", b, 'a'+i+1);
-				}
-			}						
+			// Shift data
+			memcpy( b, &(b[nChars2Send]), bIdx - nChars2Send );
+			bIdx -= nChars2Send;
+		} else if ( bIdx >= nChars2Send ) {
+			printf("Try to detect frame start ...");
+			int j;
+			for( j = 0; j < bIdx; j++ )
+			{
+				if( b[j] == ('#'+1) )
+					break;
+			}
+			memcpy( b, &(b[j]), bIdx - j );
+			bIdx -= j;
+			printf("Shifted by %d\n", j);
 		}
 		
-		double totalTime = 0;
-		double td;
-		for( int i = 0; i < N; i++ )
-		{				
-			if( tDiff[i] < 0 ){
-				printf("Strange strange ...");
-			} else {
-				totalTime += tDiff[i];
+		if( tCnter >= (N-1) )
+		{
+			tCnter = -1;
+			double totalTime = 0;
+			double td;
+			for( int i = 0; i < N; i++ )
+			{				
+				if( tDiff[i] < 0 ){
+					printf("Strange strange ...");
+				} else {
+					totalTime += tDiff[i];
+				}
 			}
-		}
-		double avgDiff =  (double) (totalTime / (double) N);
-		printf("RTT %g , N = %d , textLength %d , Sleep %d ms\n", avgDiff, N , nChars2Send, sleep_time);
+			double avgDiff =  (double) (totalTime / (double) N);
+			printf("RTT %g , N = %d , textLength %d , Sleep %d ms\n", avgDiff, N , nChars2Send, sleep_time);
+		}					
 	}
 	exit(0);
 }
